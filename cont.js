2d66e9b7ff7b15436bb321547a742a326c99d1a8
modash1311 <63658366+modash1311@users.noreply.github.com>
2020-04-26 18:20:29 +0530

    Add files via upload

---
+++ cont.js
@@ -0,0 +1,41 @@
+var getmov = [{
+      "name": "How to train your dragon: The Hidden World",
+      "img": "'/img/moimg/1c.jpg'",
+      "year": "2019",
+      "lang": "DEF-HD",
+      "trailer": "https://www.youtube.com/watch?v=SkcucKDrbOI",
+      "imdb": "https://www.imdb.com/title/tt0892769/",
+      "play": "https://jxjjxy-my.sharepoint.com/:u:/g/personal/dsznwgkc_m_mzr_me/ETJoqNgbLjFCmTYYWf5NuhcByWFnMvsSCF_08nTvkOp_xA?e=EJyvQG&download=1"
+}, {
+      "name": "Reign of the Supermen",
+      "img": "'/img/moimg/2c.jpg'",
+      "year": "2019",
+      "lang": "DEF-HD",
+      "trailer": "https://www.youtube.com/watch?v=JUM70fOy4Vk",
+      "imdb": "https://www.imdb.com/title/tt7167686/",
+      "play": "https://jxjjxy-my.sharepoint.com/:u:/g/personal/dsznwgkc_m_mzr_me/Ed_itL3iv_tIrZFkfXZROp8BEijXgFZD8BEAxPjQtnUVmQ?e=y9p2gZ&download=1"
+}, {
+      "name": "The LEGO Movie 2: The Second Part",
+      "img": "'/img/moimg/3c.jpg'",
+      "year": "2019",
+      "lang": "DEF-HD",
+      "trailer": "https://www.youtube.com/watch?v=cksYkEzUa7k",
+      "imdb": "https://www.imdb.com/title/tt3513498/",
+      "play": "https://jxjjxy-my.sharepoint.com/:u:/g/personal/dsznwgkc_m_mzr_me/ETrIB0Vi02BLsdRTPOCMq18Bvz-udnutxlu_nZ7dvmODxw?e=STTcZh&download=1"
+}, {
+      "name": "The Queen's Corgi",
+      "img": "'/img/moimg/4c.jpg'",
+      "year": "2019",
+      "lang": "DEF-HD",
+      "trailer": "https://www.youtube.com/watch?v=sbrEmlTJd7E",
+      "imdb": "https://www.imdb.com/title/tt6563576/",
+      "play": "https://jxjjxy-my.sharepoint.com/:u:/g/personal/dsznwgkc_m_mzr_me/EbjyKlZp1YZBhAzLA2TVNikBXjy2btZ26_KC6wa0gTVvDw?e=LaFhgR&download=1"
+}, {
+      "name": "Wonder Park",
+      "img": "'/img/moimg/5c.jpg'",
+      "year": "2019",
+      "lang": "DEF-HD",
+      "trailer": "https://www.youtube.com/watch?v=VML6rQWssSk",
+      "imdb": "https://www.imdb.com/title/tt6428676/",
+      "play": "https://jxjjxy-my.sharepoint.com/:u:/g/personal/dsznwgkc_m_mzr_me/EZxZ1sT3-_1NlcetqxBjcm0BNIqogqvCFkvM0CKVmAwZUw?e=AMd2hd&download=1"
+}];
